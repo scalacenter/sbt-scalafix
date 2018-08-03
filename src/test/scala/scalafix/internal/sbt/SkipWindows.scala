5047949a405082e30f79927253a66a59ef090bac
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-08-03 14:38:00 +0200

    Run CI on Windows

--- src/test/scala/scalafix/internal/sbt/SkipWindows.scala
+++ src/test/scala/scalafix/internal/sbt/SkipWindows.scala
@@ -2,4 +2,4 @@ package scalafix.internal.sbt
 
 import org.scalatest.Tag
 
-object SkipWindows extends Tag("scalafix.internal.tests.utils.SkipWindows")
+object SkipWindows extends Tag("SkipWindows")
