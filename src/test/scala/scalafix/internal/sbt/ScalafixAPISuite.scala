d85362ee17da1ba52122233c677780aa5090825e
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-08-09 14:22:59 +0200

    Remove \r\n line breaks in tests

--- src/test/scala/scalafix/internal/sbt/ScalafixAPISuite.scala
+++ src/test/scala/scalafix/internal/sbt/ScalafixAPISuite.scala
@@ -3,6 +3,7 @@ package scalafix.internal.sbt
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 import java.nio.file.Files
+import org.scalactic.source.Position
 import org.scalatest.FunSuite
 import scalafix.sbt.ScalafixPlugin
 import scala.collection.JavaConverters._
@@ -11,6 +12,15 @@ import sbt.internal.sbtscalafix.Compat
 import scalafix.interfaces.ScalafixError
 
 class ScalafixAPISuite extends FunSuite {
+
+  def assertNoDiff(obtained: String, expected: String)(
+      implicit pos: Position
+  ): Unit = {
+    def strip(s: String) = s.trim.replaceAll("\r\n", "\n")
+    val obtainedStrip = strip(obtained)
+    val expectedStrip = strip(expected)
+    assert(obtainedStrip == expectedStrip)
+  }
   test("ScalafixPlugin.cli") {
     val baos = new ByteArrayOutputStream()
     val logger = Compat.ConsoleLogger(new PrintStream(baos))
@@ -45,12 +55,12 @@ class ScalafixAPISuite extends FunSuite {
     assert(obtained.endsWith("// v1 SyntacticRule!\n"))
     val out = fansi.Str(baos.toString()).plainText
     val obtainedOut = out.replaceFirst(".*Tmp.scala", "[error] Tmp.scala")
-    assert(
-      obtainedOut.trim ==
-        """|[error] Tmp.scala:3:12: error: [DisableSyntax.noSemicolons] semicolons are disabled
-           |[error]   val x = 1;
-           |[error]            ^
-           |""".stripMargin.trim
+    assertNoDiff(
+      obtainedOut,
+      """|[error] Tmp.scala:3:12: error: [DisableSyntax.noSemicolons] semicolons are disabled
+         |[error]   val x = 1;
+         |[error]            ^
+         |""".stripMargin
     )
   }
 }
