0fef1e8d8b9f8ed8ba614543e0b2ca7a097e3bd3
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-11-29 10:54:35 +0100

    Upgrade Scalafix

--- src/main/scala/scalafix/sbt/BuildInfo.scala
+++ src/main/scala/scalafix/sbt/BuildInfo.scala
@@ -5,9 +5,9 @@ import java.util.Properties
 object BuildInfo {
 
   def scalafixVersion: String =
-    props.getProperty("scalafixVersion", "0.9.0")
+    props.getProperty("scalafixVersion", "0.9.1")
   def scalafixStableVersion: String =
-    props.getProperty("scalafixStableVersion", "0.9.0")
+    props.getProperty("scalafixStableVersion", "0.9.1")
   def scalametaVersion: String =
     props.getProperty("scalametaVersion", "4.1.0")
   def scala212: String =
