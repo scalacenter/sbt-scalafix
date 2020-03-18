b8205c5b3d433bcf6e4adeabc1ad807932a1c035
Brice Jaglin <bjaglin@gmail.com>
2020-03-18 08:10:01 +0100

    bump scalafix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val x = List(1) // scalafix:ok
-  def scalafixVersion: String = "0.9.11"
+  def scalafixVersion: String = "0.9.12"
   val all = List(
     "org.eclipse.jgit" % "org.eclipse.jgit" % "5.5.1.201910021850-r",
     "ch.epfl.scala" % "scalafix-interfaces" % scalafixVersion,
