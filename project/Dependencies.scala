7dacd3afd7e55876214f02f4d99a76f23e8b9106
Scala Steward <me@scala-steward.org>
2020-04-21 10:45:22 +0200

    Update scalafix-interfaces to 0.9.15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val x = List(1) // scalafix:ok
-  def scalafixVersion: String = "0.9.14"
+  def scalafixVersion: String = "0.9.15"
   val all = List(
     "org.eclipse.jgit" % "org.eclipse.jgit" % "5.5.1.201910021850-r",
     "ch.epfl.scala" % "scalafix-interfaces" % scalafixVersion,
