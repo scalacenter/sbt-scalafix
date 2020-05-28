c6e7d64c995926238535c50764b85c2e20d0d495
Scala Steward <me@scala-steward.org>
2020-05-28 06:40:55 +0200

    Update semanticdb-scalac to 4.3.13

--- src/main/scala/scalafix/sbt/BuildInfo.scala
+++ src/main/scala/scalafix/sbt/BuildInfo.scala
@@ -13,7 +13,7 @@ object BuildInfo {
   def scalafixStableVersion: String =
     property("scalafixStableVersion", "0.9.10")
   def scalametaVersion: String =
-    property("scalametaVersion", "4.3.10")
+    property("scalametaVersion", "4.3.13")
   def scala213: String =
     property("scala213", "2.13.1")
   def scala212: String =
