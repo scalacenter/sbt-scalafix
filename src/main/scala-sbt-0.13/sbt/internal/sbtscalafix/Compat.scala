scalacenter/sbt-scalafix

commit 568f1a2e435b225117aacae59391d505da9f1b6a

add ivy config to reference local rules compiled in another sbt project
* Handle internalDependencyClasspath & externalDependencyClasspath
  separately, so that Coursier resolves transitive dependencies of
  local & remote rules together and evicts as needed
* Tweak classloader hierarchy to keep core & global external
  dependencies warm across projects & invocations

src/main/scala/scalafix/internal/sbt/ScalafixCoursier.scala
@@ -1,6 +1,5 @@
 package scalafix.internal.sbt
 
-import java.net.URLClassLoader
 import java.nio.file.Path
 import java.util.function
 import java.{util => jutil}
@@ -33,19 +32,16 @@ object ScalafixCoursier {
 
   def scalafixToolClasspath(
       deps: Seq[ModuleID],
-      customResolvers: Seq[cs.Repository],
-      parent: ClassLoader
-  ): URLClassLoader = {
+      customResolvers: Seq[cs.Repository]
+  ): Seq[URL] = {
     if (deps.isEmpty) {
-      new URLClassLoader(Array(), parent)
+      Nil
     } else {
       val jars = dependencyCache.computeIfAbsent(
         deps,
         fetchScalafixDependencies(customResolvers)
       )
-      val urls = jars.map(_.toUri.toURL).toArray
-      val classloader = new URLClassLoader(urls, parent)
-      classloader
+      jars.map(_.toUri.toURL)
     }
   }
 

src/main/scala/scalafix/internal/sbt/ScalafixInterface.scala
@@ -7,7 +7,52 @@ import sbt._
 import sbt.internal.sbtscalafix.Compat
 import scalafix.interfaces.{ScalafixArguments, Scalafix => ScalafixAPI}
 
-case class ScalafixInterface(api: ScalafixAPI, args: ScalafixArguments)
+class ScalafixInterface private (
+    val args: ScalafixArguments,
+    private val toolClasspath: URLClassLoader
+) {
+
+  // Accumulates the classpath via classloader delegation, as args.withToolClasspath() only considers the last call.
+  //
+  // We effectively end up with the following class loader hierarchy:
+  //   1. Meta-project sbt class loader
+  //       - bound to the sbt session
+  //   2. ScalafixInterfacesClassloader, loading `scalafix-interfaces` from its parent
+  //       - bound to the sbt session
+  //   3. `scalafix-cli` JARs
+  //       - bound to the sbt session
+  //   4. Global, external dependencies
+  //       - present only if custom dependencies were defined
+  //       - used for rule names autocompletion
+  //       - bound to the sbt session
+  //   5. Project-level, internal and external classpath for ScalafixConfig ivy configuration & dependencies for
+  //      CLI-requested `dependency:` rules
+  //       - present only on project-specific (local rules) or invocation-specific (`dependency:` rule) classpath
+  //       - recreated at each task invocation
+  def addToolClasspath(
+      extraExternalDeps: Seq[ModuleID],
+      customResolvers: Seq[Repository],
+      extraInternalDeps: Seq[File]
+  ): ScalafixInterface = {
+    if (extraExternalDeps.isEmpty && extraInternalDeps.isEmpty) {
+      this
+    } else {
+      val extraURLs = ScalafixCoursier
+        .scalafixToolClasspath(
+          extraExternalDeps,
+          customResolvers
+        ) ++ extraInternalDeps.map(_.toURI.toURL)
+
+      val newToolClasspath =
+        new URLClassLoader(extraURLs.toArray, toolClasspath)
+      new ScalafixInterface(
+        args.withToolClasspath(newToolClasspath),
+        newToolClasspath
+      )
+    }
+  }
+}
+
 object ScalafixInterface {
   private class LazyValue[T](thunk: () => T) extends (() => T) {
     private lazy val _value = scala.util.Try(thunk())
@@ -25,18 +70,16 @@ object ScalafixInterface {
         new ScalafixInterfacesClassloader(this.getClass.getClassLoader)
       val classloader = new URLClassLoader(urls, interfacesParent)
       val api = ScalafixAPI.classloadInstance(classloader)
-      val toolClasspath = ScalafixCoursier.scalafixToolClasspath(
-        scalafixDependencies,
-        scalafixCustomResolvers,
-        classloader
-      )
       val callback = new ScalafixLogger(logger)
 
       val args = api
         .newArguments()
-        .withToolClasspath(toolClasspath)
         .withMainCallback(callback)
 
-      ScalafixInterface(api, args)
+      new ScalafixInterface(args, classloader).addToolClasspath(
+        scalafixDependencies,
+        scalafixCustomResolvers,
+        Nil
+      )
     })
 }

src/main/scala/scalafix/sbt/ScalafixPlugin.scala
@@ -21,6 +21,9 @@ object ScalafixPlugin extends AutoPlugin {
   object autoImport {
     val Scalafix = Tags.Tag("scalafix")
 
+    val ScalafixConfig = config("scalafix")
+      .describedAs("Dependencies required for rule execution.")
+
     val scalafix: InputKey[Unit] =
       inputKey[Unit](
         "Run scalafix rule in this project and configuration. " +
@@ -71,8 +74,13 @@ object ScalafixPlugin extends AutoPlugin {
 
   import autoImport._
 
+  override lazy val projectConfigurations: Seq[Configuration] =
+    Seq(ScalafixConfig)
+
   override lazy val projectSettings: Seq[Def.Setting[_]] =
-    Seq(Compile, Test).flatMap(c => inConfig(c)(scalafixConfigSettings(c)))
+    Seq(Compile, Test).flatMap(c => inConfig(c)(scalafixConfigSettings(c))) ++
+      inConfig(ScalafixConfig)(Defaults.configSettings) ++
+      Seq(ivyConfigurations += ScalafixConfig)
 
   override lazy val globalSettings: Seq[Def.Setting[_]] = Seq(
     initialize := {
@@ -102,33 +110,32 @@ object ScalafixPlugin extends AutoPlugin {
   private def scalafixArgs(): ScalafixArguments = scalafixInterface().args
   private def scalafixArgsFromShell(
       shell: ShellArgs,
-      baseDependencies: Seq[ModuleID],
-      baseResolvers: Seq[Repository]
-  ): (ShellArgs, ScalafixArguments) = {
+      projectDepsExternal: Seq[ModuleID],
+      baseResolvers: Seq[Repository],
+      projectDepsInternal: Seq[File]
+  ) = {
     val (dependencyRules, rules) =
       shell.rules.partition(_.startsWith("dependency:"))
-    if (dependencyRules.isEmpty) {
-      (shell, scalafixInterface().args)
-    } else {
-      val parsed = dependencyRules.map {
-        case DependencyRule.Parsed(ruleName, org, art, ver) =>
-          DependencyRule(ruleName, org %% art % ver)
-        case els =>
-          stdoutLogger.error(
-            s"""|Invalid rule:    $els
-                |Expected format: ${DependencyRule.format}
-                |""".stripMargin
-          )
-          throw new ScalafixFailed(List(ScalafixError.CommandLineError))
-      }
-      val extraDependencies = parsed.map(_.dependency)
-      val interface = ScalafixInterface.fromToolClasspath(
-        scalafixDependencies = extraDependencies ++ baseDependencies,
-        scalafixCustomResolvers = baseResolvers
-      )
-      val newShell = shell.copy(rules = rules ++ parsed.map(_.ruleName))
-      (newShell, interface().args)
+    val parsed = dependencyRules.map {
+      case DependencyRule.Parsed(ruleName, org, art, ver) =>
+        DependencyRule(ruleName, org %% art % ver)
+      case els =>
+        stdoutLogger.error(
+          s"""|Invalid rule:    $els
+              |Expected format: ${DependencyRule.format}
+              |""".stripMargin
+        )
+        throw new ScalafixFailed(List(ScalafixError.CommandLineError))
     }
+    val rulesDepsExternal = parsed.map(_.dependency)
+    val interface = scalafixInterface().addToolClasspath(
+      rulesDepsExternal ++ projectDepsExternal,
+      baseResolvers,
+      projectDepsInternal
+    )
+    val newShell = shell.copy(rules = rules ++ parsed.map(_.ruleName))
+    (newShell, interface.args)
+
   }
 
   private def scalafixInputTask(
@@ -137,17 +144,30 @@ object ScalafixPlugin extends AutoPlugin {
     Def.inputTaskDyn {
       val shell0 = new ScalafixCompletions(
         workingDirectory = () => workingDirectory,
+        // Unfortunately, local rules will not show up as completions in the parser, as that parser can only depend
+        // on global settings (see note in initialize), while local rules classpath must
+        // be looked up via tasks on projects
         loadedRules = () => scalafixArgs().availableRules().asScala,
         terminalWidth = Some(JLineAccess.terminalWidth)
       ).parser.parsed
+
+      val projectDepsInternal =
+        internalDependencyClasspath.in(ScalafixConfig).value.map(_.data)
+      val projectDepsExternal =
+        externalDependencyClasspath
+          .in(ScalafixConfig)
+          .value
+          .flatMap(_.get(moduleID.key))
+
       if (shell0.rules.isEmpty && shell0.extra == List("--help")) {
         scalafixHelp
       } else {
         val scalafixConf = scalafixConfig.value.map(_.toPath)
         val (shell, mainArgs0) = scalafixArgsFromShell(
           shell0,
-          scalafixDependencies.in(ThisBuild).value,
-          scalafixResolvers.in(ThisBuild).value
+          projectDepsExternal,
+          scalafixResolvers.in(ThisBuild).value,
+          projectDepsInternal
         )
         val mainArgs = mainArgs0
           .withConfig(jutil.Optional.ofNullable(scalafixConf.orNull))

src/test/scala/scalafix/internal/sbt/ScalafixAPISuite.scala
@@ -27,11 +27,13 @@ class ScalafixAPISuite extends AnyFunSuite {
     assume(!Properties.isWin)
     val baos = new ByteArrayOutputStream()
     val logger = Compat.ConsoleLogger(new PrintStream(baos))
-    val ScalafixInterface(_, args) = ScalafixInterface.fromToolClasspath(
-      List("com.geirsson" %% "example-scalafix-rule" % "1.3.0"),
-      ScalafixCoursier.defaultResolvers,
-      logger
-    )()
+    val args = ScalafixInterface
+      .fromToolClasspath(
+        List("com.geirsson" %% "example-scalafix-rule" % "1.3.0"),
+        ScalafixCoursier.defaultResolvers,
+        logger
+      )()
+      .args
     val tmp = Files.createTempFile("scalafix", "Tmp.scala")
     tmp.toFile.deleteOnExit()
     Files.write(

src/sbt-test/sbt-scalafix/local-rules/build.sbt
@@ -1,0 +1,15 @@
+import _root_.scalafix.sbt.{BuildInfo => Versions}
+
+val rules = project
+  .disablePlugins(ScalafixPlugin)
+  .settings(
+    scalaVersion := Versions.scala212,
+    libraryDependencies += "ch.epfl.scala" %% "scalafix-core" % Versions.scalafixVersion,
+    libraryDependencies += "joda-time" % "joda-time" % "2.10.6"
+  )
+
+val service = project
+  .dependsOn(rules % ScalafixConfig)
+  .settings(
+    scalaVersion := Versions.scala212
+  )

src/sbt-test/sbt-scalafix/local-rules/project/plugins.sbt
@@ -1,0 +1,2 @@
+resolvers += Resolver.sonatypeRepo("public")
+addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % sys.props("plugin.version"))

src/sbt-test/sbt-scalafix/local-rules/rules/src/main/resources/META-INF/services/scalafix.v1.Rule
@@ -1,0 +1,1 @@
+local.Syntactic

src/sbt-test/sbt-scalafix/local-rules/rules/src/main/scala/local/NoOp.scala
@@ -1,0 +1,10 @@
+package local
+
+import scalafix.v1._
+
+class Syntactic extends SyntacticRule("LocalSyntacticRule") {
+  override def fix(implicit doc: SyntacticDocument): Patch = {
+    new org.joda.time.Instant() // let's pretent we need this for the rule logic
+    Patch.empty
+  }
+}

commit d81dd7266231e70af475551b563b21ec75c89482

delegate rules fetching and classloading to scalafix-interfaces
The class loading hierarchy effectively becomes one level flatter,
as the potential (5) has now (3) as parent, which unfortunately results
in a small regression from 568f1a2 in case `refreshClasspath` is
true, which happens for invocations that either:
 (1) request external rules via CLI (`dependency:`)
 (2) use local rules (Scalafix ivy config)

In this case the potential `scalafixDependencies` are unnecessarily
fetched & reloaded each time (see `scalafixArgsFromShell()`).

If needed, we can later memoize the state of the `scalafixArguments`
instance containing that classloader across invocations if only (2)
is involved, relying on stamping local rules bytecode to detect when
invalidation is needed.

For (1), the performance penalty is not a big deal anyway as this is
probably meant to be a one-shot run, as the user with some
`scalafixDependencies` will probably move away that CLI-syntax if
it is meant to be permanent.

src/main/scala-sbt-0.13/sbt/internal/sbtscalafix/Compat.scala
@@ -6,6 +6,8 @@ object Compat {
   type ConsoleLogger = sbt.ConsoleLogger
   val ConsoleLogger = sbt.ConsoleLogger
 
+  val Disabled = sbt.CrossVersion.Disabled
+
   def append(
       project: Extracted,
       settings: Seq[Setting[_]],

src/main/scala-sbt-1.0/sbt/internal/sbtscalafix/Compat.scala
@@ -6,6 +6,9 @@ object Compat {
   type ConsoleLogger = sbt.internal.util.ConsoleLogger
   val ConsoleLogger = sbt.internal.util.ConsoleLogger
 
+  // https://github.com/sbt/sbt/issues/4977
+  val Disabled = sbt.librarymanagement.Disabled()
+
   def append(
       project: Extracted,
       settings: Seq[Setting[_]],

src/main/scala/scalafix/internal/sbt/ScalafixCoursier.scala
@@ -1,102 +1,0 @@
-package scalafix.internal.sbt
-
-import java.nio.file.Path
-import java.util.function
-import java.{util => jutil}
-
-import coursierapi._
-import sbt._
-import scalafix.sbt.BuildInfo
-import scala.collection.JavaConverters._
-
-object ScalafixCoursier {
-  private def scalafixCliModule: Module =
-    Module.of(
-      "ch.epfl.scala",
-      s"scalafix-cli_${BuildInfo.scala212}"
-    )
-  private def scalafixCli: Dependency =
-    Dependency.of(
-      scalafixCliModule,
-      BuildInfo.scalafixVersion
-    )
-
-  def scalafixCliJars(
-      repositories: Seq[Repository]
-  ): List[Path] = {
-    runFetch(
-      newFetch()
-        .addDependencies(scalafixCli)
-        .addRepositories(repositories: _*)
-    )
-  }
-
-  def scalafixToolClasspath(
-      deps: Seq[ModuleID],
-      customResolvers: Seq[Repository]
-  ): Seq[URL] = {
-    if (deps.isEmpty) {
-      Nil
-    } else {
-      val jars = dependencyCache.computeIfAbsent(
-        deps,
-        fetchScalafixDependencies(customResolvers)
-      )
-      jars.map(_.toUri.toURL)
-    }
-  }
-
-  private val dependencyCache: jutil.Map[Seq[ModuleID], List[Path]] = {
-    jutil.Collections.synchronizedMap(new jutil.HashMap())
-  }
-  private[scalafix] def fetchScalafixDependencies(
-      customResolvers: Seq[Repository]
-  ): function.Function[Seq[ModuleID], List[Path]] =
-    new function.Function[Seq[ModuleID], List[Path]] {
-      override def apply(t: Seq[ModuleID]): List[Path] = {
-        val dependencies = t.map { module =>
-          val binarySuffix =
-            if (module.crossVersion.isInstanceOf[CrossVersion.Binary]) "_2.12"
-            else ""
-          Dependency.of(
-            module.organization,
-            module.name + binarySuffix,
-            module.revision
-          )
-        }
-        runFetch(
-          newFetch()
-            .addRepositories(customResolvers: _*)
-            .addDependencies(scalafixCli)
-            .addDependencies(dependencies: _*)
-        )
-      }
-    }
-
-  def runFetch(fetch: Fetch): List[Path] =
-    fetch.fetch().asScala.iterator.map(_.toPath()).toList
-  def newFetch(): Fetch =
-    Fetch
-      .create()
-      .withRepositories()
-      .withResolutionParams(
-        ResolutionParams
-          .create()
-          .withForceVersions(
-            Map(
-              scalafixCliModule -> scalafixCli.getVersion()
-            ).asJava
-          )
-      )
-
-  val defaultResolvers: Seq[Repository] = Seq(
-    Repository.ivy2Local(),
-    Repository.central(),
-    coursierapi.MavenRepository.of(
-      "https://oss.sonatype.org/content/repositories/public"
-    ),
-    coursierapi.MavenRepository.of(
-      "https://oss.sonatype.org/content/repositories/snapshots"
-    )
-  )
-}

src/main/scala/scalafix/internal/sbt/ScalafixInterface.scala
@@ -1,6 +1,5 @@
 package scalafix.internal.sbt
 
-import java.net.URLClassLoader
 import java.nio.file.Path
 import java.{util => jutil}
 
@@ -19,12 +18,35 @@ object Arg {
 
   sealed trait CacheKey
 
-  case class ToolClasspath(classLoader: URLClassLoader)
-      extends Arg
+  case class ToolClasspath(
+      customURLs: Seq[URL],
+      customDependencies: Seq[ModuleID],
+      repositories: Seq[Repository]
+  ) extends Arg
       with CacheKey {
     override def apply(sa: ScalafixArguments): ScalafixArguments =
-      // this effectively overrides any previous URLClassLoader
-      sa.withToolClasspath(classLoader)
+      sa.withToolClasspath(
+        customURLs.asJava,
+        customDependencies.map(_.asCoursierCoordinates).asJava,
+        repositories.asJava
+      )
+
+    private implicit class XtensionModuleID(m: ModuleID) {
+      def asCoursierCoordinates: String = {
+        m.crossVersion match {
+          case Compat.Disabled =>
+            s"${m.organization}:${m.name}:${m.revision}"
+          case _: CrossVersion.Binary =>
+            s"${m.organization}::${m.name}:${m.revision}"
+          case _: CrossVersion.Full =>
+            s"${m.organization}:::${m.name}:${m.revision}"
+          case other =>
+            throw new InvalidArgument(
+              s"Unsupported crossVersion $other for dependency $m"
+            )
+        }
+      }
+    }
   }
 
   case class Rules(rules: Seq[String]) extends Arg with CacheKey {
@@ -78,64 +100,6 @@ class ScalafixInterface private (
     val args: Seq[Arg]
 ) {
 
-  private val lastToolClasspath = args.reverse
-    .collectFirst { case tcp: Arg.ToolClasspath => tcp }
-    .getOrElse(
-      throw new IllegalArgumentException(
-        "a base toolClasspath must be provided"
-      )
-    )
-    .classLoader
-
-  private def this(
-      api: ScalafixAPI,
-      toolClasspath: URLClassLoader,
-      mainCallback: ScalafixMainCallback
-  ) =
-    this(
-      api
-        .newArguments()
-        .withMainCallback(mainCallback)
-        .withToolClasspath(toolClasspath),
-      Seq(Arg.ToolClasspath(toolClasspath))
-    )
-
-  // Accumulates the classpath via classloader delegation, as only the last Arg.ToolClasspath is considered
-  //
-  // We effectively end up with the following class loader hierarchy:
-  //   1. Meta-project sbt class loader
-  //       - bound to the sbt session
-  //   2. ScalafixInterfacesClassloader, loading `scalafix-interfaces` from its parent
-  //       - bound to the sbt session
-  //   3. `scalafix-cli` JARs
-  //       - passed in the constructor
-  //       - bound to the sbt session
-  //   4. Global, external dependencies
-  //       - present only if custom dependencies were defined
-  //       - used for rule names autocompletion
-  //       - bound to the sbt session
-  //   5. Project-level, internal and external classpath for ScalafixConfig ivy configuration & dependencies for
-  //      CLI-requested `dependency:` rules
-  //       - present only on project-specific (local rules) or invocation-specific (`dependency:` rule) classpath
-  //       - recreated at each task invocation
-  def addToolClasspath(
-      extraExternalDeps: Seq[ModuleID],
-      customResolvers: Seq[Repository],
-      extraInternalDeps: Seq[File]
-  ): ScalafixInterface = {
-    val extraURLs = ScalafixCoursier
-      .scalafixToolClasspath(
-        extraExternalDeps,
-        customResolvers
-      ) ++ extraInternalDeps.map(_.toURI.toURL)
-
-    if (extraURLs.isEmpty) this
-    else {
-      val classpath = new URLClassLoader(extraURLs.toArray, lastToolClasspath)
-      withArgs(Arg.ToolClasspath(classpath))
-    }
-  }
-
   def withArgs(args: Arg*): ScalafixInterface = {
     val newScalafixArguments = args.foldLeft(scalafixArguments) { (acc, arg) =>
       try arg(acc)
@@ -170,18 +134,18 @@ object ScalafixInterface {
       logger: Logger = Compat.ConsoleLogger(System.out)
   ): () => ScalafixInterface =
     new LazyValue({ () =>
-      val jars = ScalafixCoursier.scalafixCliJars(scalafixCustomResolvers)
-      val urls = jars.map(_.toUri.toURL).toArray
-      val interfacesParent =
-        new ScalafixInterfacesClassloader(this.getClass.getClassLoader)
-      val classloader = new URLClassLoader(urls, interfacesParent)
-      val api = ScalafixAPI.classloadInstance(classloader)
       val callback = new ScalafixLogger(logger)
-      new ScalafixInterface(api, classloader, callback)
-        .addToolClasspath(
-          scalafixDependencies,
-          scalafixCustomResolvers,
-          Nil
+      val scalafixArguments = ScalafixAPI
+        .fetchAndClassloadInstance("2.12", scalafixCustomResolvers.asJava)
+        .newArguments()
+        .withMainCallback(callback)
+      new ScalafixInterface(scalafixArguments, Nil)
+        .withArgs(
+          Arg.ToolClasspath(
+            Nil,
+            scalafixDependencies,
+            scalafixCustomResolvers
+          )
         )
     })
 }

src/main/scala/scalafix/internal/sbt/ScalafixInterfacesClassloader.scala
@@ -1,19 +1,0 @@
-package scalafix.internal.sbt
-
-/**
-  * A classloader that shares only scalafix-interfaces classes from the parent classloader.
-  *
-  * This classloader is intended to be used as a parent when class-loading scalafix-cli.
-  * By using this classloader as a parent, it's possible to cast runtime instances from
-  * the scalafix-cli classloader into `scalafix.interfaces.Scalafix` from this classlaoder.
-  */
-class ScalafixInterfacesClassloader(parent: ClassLoader)
-    extends ClassLoader(null) {
-  override def findClass(name: String): Class[_] = {
-    if (name.startsWith("scalafix.interfaces")) {
-      parent.loadClass(name)
-    } else {
-      throw new ClassNotFoundException(name)
-    }
-  }
-}

src/main/scala/scalafix/sbt/ScalafixPlugin.scala
@@ -13,6 +13,7 @@ import sbt.internal.sbtscalafix.Caching._
 import sbt.internal.sbtscalafix.{Compat, JLineAccess}
 import sbt.plugins.JvmPlugin
 import scalafix.interfaces.ScalafixError
+import scalafix.internal.sbt.Arg.ToolClasspath
 import scalafix.internal.sbt._
 
 import scala.util.Try
@@ -129,7 +130,15 @@ object ScalafixPlugin extends AutoPlugin {
   override lazy val globalSettings: Seq[Def.Setting[_]] = Seq(
     scalafixConfig := None, // let scalafix-cli try to infer $CWD/.scalafix.conf
     scalafixCaching := false,
-    scalafixResolvers := ScalafixCoursier.defaultResolvers,
+    scalafixResolvers := Seq(
+      Repository.ivy2Local(),
+      Repository.central(),
+      coursierapi.MavenRepository
+        .of("https://oss.sonatype.org/content/repositories/public"),
+      coursierapi.MavenRepository.of(
+        "https://oss.sonatype.org/content/repositories/snapshots"
+      )
+    ),
     scalafixDependencies := Nil,
     commands += ScalafixEnable.command
   )
@@ -158,6 +167,7 @@ object ScalafixPlugin extends AutoPlugin {
       shell: ShellArgs,
       scalafixInterface: () => ScalafixInterface,
       projectDepsExternal: Seq[ModuleID],
+      baseDepsExternal: Seq[ModuleID],
       baseResolvers: Seq[Repository],
       projectDepsInternal: Seq[File]
   ): (ShellArgs, ScalafixInterface) = {
@@ -175,11 +185,22 @@ object ScalafixPlugin extends AutoPlugin {
         throw new ScalafixFailed(List(ScalafixError.CommandLineError))
     }
     val rulesDepsExternal = parsed.map(_.dependency)
-    val interface = scalafixInterface().addToolClasspath(
-      rulesDepsExternal ++ projectDepsExternal,
-      baseResolvers,
-      projectDepsInternal
-    )
+    val refreshClasspath =
+      (projectDepsInternal ++ projectDepsExternal ++ rulesDepsExternal).nonEmpty
+    val interface =
+      if (refreshClasspath)
+        scalafixInterface().withArgs(
+          ToolClasspath(
+            projectDepsInternal.map(_.toURI.toURL),
+            baseDepsExternal ++ projectDepsExternal ++ rulesDepsExternal,
+            baseResolvers
+          )
+        )
+      else
+        // if there is nothing specific to the project or the invocation, reuse the default
+        // interface which already has the baseDepsExternal loaded
+        scalafixInterface()
+
     val newShell = shell.copy(rules = rules ++ parsed.map(_.ruleName))
     (newShell, interface)
 
@@ -244,6 +265,7 @@ object ScalafixPlugin extends AutoPlugin {
           shellArgs,
           scalafixInterface.value,
           projectDepsExternal,
+          scalafixDependencies.in(ThisBuild).value,
           scalafixResolvers.in(ThisBuild).value,
           projectDepsInternal
         )
@@ -346,8 +368,8 @@ object ScalafixPlugin extends AutoPlugin {
 
       implicit val stamper = new CacheKeysStamper {
         override protected def stamp: Arg.CacheKey => Unit = {
-          case Arg.ToolClasspath(classLoader) =>
-            val files = classLoader.getURLs
+          case Arg.ToolClasspath(customURLs, customDependencies, _) =>
+            val files = customURLs
               .map(url => Paths.get(url.toURI).toFile)
               .flatMap {
                 case classDirectory if classDirectory.isDirectory =>
@@ -356,6 +378,7 @@ object ScalafixPlugin extends AutoPlugin {
                   Seq(jar)
               }
             write(files.map(FileInfo.lastModified.apply))
+            write(customDependencies.map(_.toString))
           case Arg.Rules(rules) =>
             rules.foreach {
               case source

src/test/scala/scalafix/internal/sbt/SbtCompletionsSuite.scala
@@ -1,5 +1,6 @@
 package scalafix.internal.sbt
 
+import coursierapi.Repository
 import org.eclipse.jgit.lib.AbbreviatedObjectId
 import org.scalatest.Tag
 import sbt.complete.Parser
@@ -30,7 +31,7 @@ class SbtCompletionsSuite extends AnyFunSuite {
     ScalafixInterface
       .fromToolClasspath(
         Seq(exampleDependency),
-        ScalafixCoursier.defaultResolvers
+        Seq(Repository.central)
       )()
   val loadedRules = mainArgs.availableRules.toList
 

src/test/scala/scalafix/internal/sbt/ScalafixAPISuite.scala
@@ -3,12 +3,13 @@ package scalafix.internal.sbt
 import java.io.{ByteArrayOutputStream, PrintStream}
 import java.nio.file.Files
 
+import coursierapi.Repository
 import org.scalactic.source.Position
 import sbt._
 import sbt.internal.sbtscalafix.Compat
 import scalafix.interfaces.ScalafixError
-
 import org.scalatest.funsuite.AnyFunSuite
+
 import scala.util.Properties
 
 class ScalafixAPISuite extends AnyFunSuite {
@@ -29,7 +30,7 @@ class ScalafixAPISuite extends AnyFunSuite {
     val interface = ScalafixInterface
       .fromToolClasspath(
         List("ch.epfl.scala" %% "example-scalafix-rule" % "1.4.0"),
-        ScalafixCoursier.defaultResolvers,
+        Seq(Repository.central),
         logger
       )()
       .withArgs(Arg.PrintStream(new PrintStream(baos)))