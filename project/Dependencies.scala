c279839d81add322100ff41a55f1b9532dce3ef3
Meriam Lachkar <meriam.lachkar@gmail.com>
2021-11-26 14:26:20 +0100

    Release scalafix 0.9.33 (#261)
    * Release scalafix 0.9.33
    
    * CMSClassUnloadingEnabled is removed since jdk 15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val x = List(1) // scalafix:ok
-  def scalafixVersion: String = "0.9.32"
+  def scalafixVersion: String = "0.9.33"
 
   val all = List(
     "org.eclipse.jgit" % "org.eclipse.jgit" % "5.13.0.202109080827-r",
