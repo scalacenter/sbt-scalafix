c433c2ec5fc5f9a58dcb72b85127e97a174fc253
Scala Steward <scala_steward@virtuslab.com>
2022-07-15 21:31:29 +0000

    Update org.eclipse.jgit to 5.13.1.202206130422-r

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   def scalafixVersion: String = "0.10.1"
 
   val all = List(
-    "org.eclipse.jgit" % "org.eclipse.jgit" % "5.13.0.202109080827-r",
+    "org.eclipse.jgit" % "org.eclipse.jgit" % "5.13.1.202206130422-r",
     "ch.epfl.scala" % "scalafix-interfaces" % scalafixVersion,
     "io.get-coursier" % "interface" % "1.0.7"
   )
