0d954e97a61af7aa16463630a1b9c87c09e96781
Brice Jaglin <bjaglin@gmail.com>
2022-09-20 23:55:43 +0200

    scalafix 0.10.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val x = List(1) // scalafix:ok
-  def scalafixVersion: String = "0.10.2"
+  def scalafixVersion: String = "0.10.3"
 
   val all = List(
     "org.eclipse.jgit" % "org.eclipse.jgit" % "5.13.1.202206130422-r",
