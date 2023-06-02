8c6a31b4f506ff9067795466c5ad3921dd998471
Brice Jaglin <bjaglin@gmail.com>
2023-06-02 08:45:35 +0200

    Latest scalafix snapshot

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val x = List(1) // scalafix:ok
-  def scalafixVersion: String = "0.10.4+78-f6b8c84d-SNAPSHOT"
+  def scalafixVersion: String = "0.10.4+150-30bbae7d-SNAPSHOT"
 
   val all = List(
     "org.eclipse.jgit" % "org.eclipse.jgit" % "5.13.1.202206130422-r",
