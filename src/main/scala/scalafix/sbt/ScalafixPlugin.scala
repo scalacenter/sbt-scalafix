07667ba4ef8777fb09feb688fa82c651b2532bbf
GreyPlane <709327148@qq.com>
2024-03-12 08:31:01 +0800

    refactor: comments

--- src/main/scala/scalafix/sbt/ScalafixPlugin.scala
+++ src/main/scala/scalafix/sbt/ScalafixPlugin.scala
@@ -162,7 +162,7 @@ object ScalafixPlugin extends AutoPlugin {
       )
       if (adaptedRepo.isEmpty) {
         stdoutLogger.warn(
-          s"Defined resolver $resolver cannot be converted to coursier repository, thus will be ignored by scalafix."
+          s"Defined resolver $resolver cannot be converted to coursier repository, it will be ignored by scalafix."
         )
       }
       adaptedRepo
@@ -332,7 +332,7 @@ object ScalafixPlugin extends AutoPlugin {
         workingDirectory = (ThisBuild / baseDirectory).value.toPath,
         loadedRules = () =>
           scalafixInterfaceProvider
-            // sbt Credentials is task, so unfortunately it cannot be showed up here
+            // sbt Credentials is a task, so unfortunately it cannot be showed up here
             .value(scalafixResolvers.value)
             .availableRules(),
         terminalWidth = Some(JLineAccess.terminalWidth),
@@ -387,7 +387,7 @@ object ScalafixPlugin extends AutoPlugin {
       new ScalafixCompletions(
         workingDirectory = (ThisBuild / baseDirectory).value.toPath,
         loadedRules = () =>
-          // sbt Credentials is task, so unfortunately it cannot be showed up here
+          // sbt Credentials is a task, so unfortunately it cannot be showed up here
           scalafixInterfaceProvider
             .value(scalafixResolvers.value)
             .availableRules(),
