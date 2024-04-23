39b662f796159cb5d22fd72f8a385e38b214b100
Brice Jaglin <bjaglin@gmail.com>
2024-04-23 23:13:18 +0200

    drop sbt 1.3.x support

--- src/main/scala/sbt/internal/sbtscalafix/JLineAccess.scala
+++ src/main/scala/sbt/internal/sbtscalafix/JLineAccess.scala
@@ -2,5 +2,5 @@ package sbt.internal.sbtscalafix
 
 /** Helper class to access sbt's JLine instance */
 object JLineAccess {
-  def terminalWidth: Int = sbt.internal.util.JLine.usingTerminal(_.getWidth)
+  def terminalWidth: Int = sbt.internal.util.Terminal.get.getWidth
 }
